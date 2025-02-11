23e9f0feb6dbc596f6618229ef0d27991cf26665
AleFalces <alefalces@gmail.com>
2025-02-11 09:20:32 -0300

    axios and clear code

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -1,14 +1,14 @@
 
 const renderCards = require('./renderCards')
+const axios = require('axios')
 
-
-
-$.get("https://students-api.up.railway.app/movies", (data, status) => {
-  renderCards(data)
-  });
+// $.get("https://students-api.up.railway.app/movies", (data, status) => {
+//   renderCards(data)
+//   });
   
 
-
-
-
- 
+let fetchData = async () => {
+  let response = await axios.get('https://students-api.up.railway.app/movies')
+  renderCards(response.data)
+}
+fetchData()
