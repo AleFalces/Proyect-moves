146fe0f20e476778ed89317a33010c5ca1ec93f2
AleFalces <alefalces@gmail.com>
2025-02-20 13:22:51 -0300

    all Styles

--- front/scripts/renderCards.js
+++ front/scripts/renderCards.js
@@ -4,10 +4,10 @@ const renderCards = (data) => {
     let theCard = document.createElement("div");
     theCard.innerHTML = `
       <h2 class="card-title">${movie.title}</h2>
-      <p class="card-text">${movie.director}</p>
-      <p class="card-text">${movie.duration}</p>
-      <p class="card-text">${movie.genre}</p>
-      <p class="card-text">${movie.rate}</p>
+      <p class="card-text"> Director: ${movie.director}</p>
+      <p class="card-text"> Duration: ${movie.duration}</p>
+      <p class="card-text"> Genres: ${movie.genre}</p>
+      <p class="card-text"> Rate: ${movie.rate} ⭐</p>
       <img src="${movie.poster}" alt="${movie.title}"  class="img" >`;
     theCard.classList.add("theCard");
     cardContainer.appendChild(theCard);
@@ -15,33 +15,3 @@ const renderCards = (data) => {
 };
 
 module.exports = renderCards;
-
-//   Tag X tag option
-/* const cardContainer = document.getElementById('container') 
-    title.classList.add('card')
-    title.innerText =movie.title
-    
-     let title = document.createElement('article')
-    
-    let director = document.createElement('article')
-    director.innerText = movie.director
-    
-    let duration = document.createElement('article')
-    duration.innerText = movie.duration
-    
-    let genre = document.createElement('article')
-    genre.innerText = movie.genre
-    
-    let rate = document.createElement('article')
-    rate.innerText = movie.rate
-    
-    let poster = document.createElement('img')
-    poster.src = movie.poster
-  
-    card.appendChild(title)
-    card.appendChild(director) 
-    card.appendChild(duration)
-    card.appendChild(genre)
-    card.appendChild(rate)
-    card.appendChild(poster) 
-      */
